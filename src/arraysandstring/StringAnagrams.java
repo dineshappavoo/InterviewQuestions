06ded475397c8b53c7dd9ec319b9fcadffc62dc4
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-04-21 00:20:39 -0500

    Added comments

--- src/arraysandstring/StringAnagrams.java
+++ src/arraysandstring/StringAnagrams.java
@@ -15,14 +15,17 @@ public class StringAnagrams {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		boolean result=new StringAnagrams().isAnagram("DAny", "dany");
+		boolean result=new StringAnagrams().isAnagram("fAny", "dany");
 		System.out.println("Is Anagram : "+result);
 	}
 	
 	public boolean isAnagram(String str1, String str2)
 	{
+		//To get the no of occurrences of each character and store it in their ASCII location
 		int[] strCountArr1=getCountASCII(str1);
 		int[] strCountArr2=getCountASCII(str2);
+		
+		//To Test whether the two arrays have the same count of characters. Array size 256 since ASCII 256 unique values
 		for(int i=0;i<256;i++)
 		{
 			if(strCountArr1[i]!=strCountArr2[i])
@@ -37,11 +40,13 @@ public class StringAnagrams {
 	public int[] getCountASCII(String str)
 	{
 		char c;
+		//Array size 256 for ASCII
 		int[] strCountArr=new int[256];
 		for(int i=0;i<str.length();i++)
 		{
-			c=Character.toUpperCase(str.charAt(i)); // If both the cases are considered to be the same
-			strCountArr[(int)c]++;
+			c=str.charAt(i); 
+			c=Character.toUpperCase(c);// If both the cases are considered to be the same
+			strCountArr[(int)c]++; //To increment the count in the character's ASCII location
 		}
 		return strCountArr;
 	}
