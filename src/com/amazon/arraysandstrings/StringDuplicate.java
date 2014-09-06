0847c2fa8f15b596fa5c4c8ca96b345a112035a3
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-09-06 03:37:24 -0500

    Code to verify whether a string has duplicates

--- src/com/amazon/arraysandstrings/StringDuplicate.java
+++ src/com/amazon/arraysandstrings/StringDuplicate.java
@@ -3,6 +3,8 @@
  */
 package com.amazon.arraysandstrings;
 
+import java.util.HashMap;
+
 /**
  * @author Dinesh Appavoo
  *
@@ -13,6 +15,20 @@ public class StringDuplicate {
 
 	}
 	
-	public 
+	public boolean isDuplicateExist(String str)
+	{
+		if(str.length()>256)
+			return false;
+		HashMap<Character, Boolean> hMap=new HashMap<Character, Boolean>();
+		for(int i=0;i<str.length();i++)
+		{
+			char c=str.charAt(i);
+			if(hMap.get(c))
+				return false;
+			else
+				hMap.put(c, true);
+		}
+		return true;
+	}
 
 }
