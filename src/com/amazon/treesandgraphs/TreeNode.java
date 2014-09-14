409ca74a3ab1c1e57fba9ef9face6ad58f6754e9
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-09-14 03:42:58 -0500

    Tree implementation in java

---
+++ src/com/amazon/treesandgraphs/TreeNode.java
@@ -0,0 +1,158 @@
+/**
+ * 
+ */
+package com.amazon.treesandgraphs;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author Dinesh Appavoo
+ *
+ */
+public class TreeNode<key, value> implements Iterable<key> {
+
+	private Node root;
+	private int N;
+	class Node{
+		key key;
+		value value;
+		boolean visited;
+		ArrayList<Node> adjacencyList=null;
+
+		public Node(key key, value value)
+		{
+			this.key=key;
+			this.value=value;
+			this.visited=false;
+			this.adjacencyList=new ArrayList<Node>();
+		}		
+
+		public ArrayList<Node> getAdjacencyList()
+		{
+			return this.adjacencyList;
+		}
+	}
+
+	public int getSize()
+	{
+		return N;
+	}
+
+	public void put(key key, value value, key parent)
+	{
+		Queue<Node> queue=new LinkedList<Node>();
+		boolean keyExists=false;
+		queue.add(root);
+		while(!queue.isEmpty())
+		{
+			Node n=queue.poll();
+			if(n.key.equals(parent))
+			{
+				ArrayList<Node> adjacentList=n.getAdjacencyList();
+				for(Node nd : adjacentList)
+				{
+					if(nd.equals(key))
+					{
+						nd.value=value;
+						keyExists=true;
+					}
+				}
+				if(!keyExists)
+				{
+					adjacentList.add(new Node(key, value));
+				}
+				break;
+			}
+			ArrayList<Node> adjList=n.getAdjacencyList();
+			for(Node node : adjList)
+			{
+				queue.add(node);
+			}
+		}
+	}
+
+	public value get(key key)
+	{
+		Queue<Node> queue=new LinkedList<Node>();
+		queue.add(root);
+		while(!queue.isEmpty())
+		{
+			Node n=queue.poll();
+			if(n.key.equals(key))
+			{
+				return n.value;
+			}
+			ArrayList<Node> adjList=n.getAdjacencyList();
+			for(Node node : adjList)
+			{
+				queue.add(node);
+			}
+		}
+		return null;
+	}
+
+	public value remove(key key)
+	{
+		Queue<Node> queue=new LinkedList<Node>();
+		queue.add(root);
+		if(root.key.equals(key))
+		{
+			value tempValue=root.value;
+			root=null;
+			return tempValue;
+		}
+		while(!queue.isEmpty())
+		{
+			Node n=queue.poll();
+
+			ArrayList<Node> adjList=n.getAdjacencyList();
+			for(Node node : adjList)
+			{
+				if(node.key.equals(key))
+				{
+					value tempVal=node.value;
+					n.adjacencyList.remove(node);
+					return tempVal;
+				}
+				queue.add(node);
+			}
+		}
+		return null;
+	}
+
+	public boolean contains(key key)
+	{
+
+		Queue<Node> queue=new LinkedList<Node>();
+		queue.add(root);
+		if(root.key.equals(key))
+		{
+			return true;
+		}
+		while(!queue.isEmpty())
+		{
+			Node n=queue.poll();
+
+			ArrayList<Node> adjList=n.getAdjacencyList();
+			for(Node node : adjList)
+			{
+				if(node.key.equals(key))
+				{
+					return true;
+				}
+				queue.add(node);
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	public Iterator<key> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
