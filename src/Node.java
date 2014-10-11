7c9fd4ff4e5519ee0f43043b903d6c3f68647cd0
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-10-11 00:01:36 -0500

    ff

---
+++ src/Node.java
@@ -0,0 +1,14 @@
+
+
+
+class Node {
+   int id;
+   int parentId;
+   int weight;
+
+   Node(int id, int parentId, int weight){
+      this.id = id;
+      this.parentId = parentId;
+      this.weight = weight;
+}
+}
