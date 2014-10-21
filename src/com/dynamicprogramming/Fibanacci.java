8931d65613c03643e76cbc84f4c8e6d15a2fb2cc
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-10-20 22:16:10 -0500

    Fibonacci

---
+++ src/com/dynamicprogramming/Fibanacci.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.dynamicprogramming;
+
+import java.util.HashMap;
+
+import javax.xml.crypto.dsig.spec.HMACParameterSpec;
+
+/**
+ * @author Dany
+ *
+ */
+public class Fibanacci {
+
+	/**
+	 * @param args
+	 */
+	private static HashMap<Integer, Integer> memoTable=new HashMap<Integer, Integer>();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int res=new Fibanacci().fibonacci(8);
+		System.out.println(res);
+
+	}
+	
+	public int fibonacci(int n)
+	{
+		if(n<0)
+			return -1;
+		if(n==0)
+			return 0;
+		if(n==1)
+			return 1;
+		if(memoTable.containsKey(n))
+		{
+			return memoTable.get(n);
+		}else
+		{
+			memoTable.put(n, (fibonacci(n-1)+fibonacci(n-2)));
+		}
+		return memoTable.get(n);
+	}
+	
+	
+
+}
