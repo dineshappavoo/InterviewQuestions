569aeaf170f5f89a43ceef24b10ca2eaa5188de8
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-10-21 04:21:02 -0500

    Updated to print the ways

--- src/com/dynamicprogramming/ChildRunnignSteps.java
+++ src/com/dynamicprogramming/ChildRunnignSteps.java
@@ -14,31 +14,31 @@ public class ChildRunnignSteps {
 	/**
 	 * @param args
 	 */
-	
+
 	static int count=0;
 	static HashMap<Integer, Integer> memoTable=new HashMap<Integer, Integer>();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int res = new ChildRunnignSteps().findSteps(4);
-		//System.out.println(count);
-		System.out.println(res);
+		new ChildRunnignSteps().findSteps(20);
+		System.out.println(count);
+		//System.out.println(res);
 
 	}
-	
-	public int findSteps(int n)
+
+	public void findSteps(int n)
 	{
 		if(n==0)
-			return 0;
+			return;
 		else
-			return fStepsUsingDP(n);
-			
+			fSteps(n,n,0,new HashMap<Integer, String>());
+
 	}
 	public int fStepsUsingDP(int n)
 	{
 		if(n<0)
 			return 0;
-		
+
 		if(n==0)
 		{
 			count++;
@@ -53,21 +53,33 @@ public class ChildRunnignSteps {
 		}
 		return memoTable.get(n);
 	}
-	
-	public void fSteps(int n)
+
+	public void fSteps(int n, int origN, int level, HashMap<Integer, String> ways)
 	{
 		if(n<0)
 			return;
-		
+
 		if(n==0)
 		{
+			ways.put(level, (origN-n)+"");
 			count++;
+			printWays(ways, level);
 			return;
 		}
 
-		fStepsUsingDP(n-1);
-		fStepsUsingDP(n-2);
-		fStepsUsingDP(n-3);
+		ways.put(level, ""+(origN-n));
+		fSteps(n-1, n, level+1, ways);
+		fSteps(n-2, n, level+1, ways);
+		fSteps(n-3, n, level+1, ways);
+	}
+
+	public void printWays(HashMap<Integer, String> ways, int level)
+	{
+		for(int i=0;i<=level;i++)
+		{
+			System.out.print(ways.get(i)+",");
+		}
+		System.out.println();
 	}
 
 }
